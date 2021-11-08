f2afd5c08e84f1f6b7ab853b3523d05d9b31831b
Arda Rugji <swapnil@legodesk.com>
2021-11-08 21:03:28 +0530

    helper file updated

--- src/main/java/controller/TaskHelper.java
+++ src/main/java/controller/TaskHelper.java
@@ -16,63 +16,62 @@ import javax.persistence.TypedQuery;
 import model.Task;
 
 public class TaskHelper {
-    private static EntityManagerFactory tasksEMF = Persistence.createEntityManagerFactory("TasklistPlanner");
-    
-    public Task searchForTasksById(int id) {
-        EntityManager tasksEM = tasksEMF.createEntityManager();
-        tasksEM.getTransaction().begin();
-        
-        Task foundTask = tasksEM.find(Task.class, id);
-        
-        tasksEM.close();
-        return foundTask;
-    }
-    
-    public void insertTask(Task task) {
-        EntityManager tasksEM = tasksEMF.createEntityManager();
-        tasksEM.getTransaction().begin();
-        
-        tasksEM.persist(task);
-        tasksEM.getTransaction().commit();
-        tasksEM.close();
-    }
-    
-    public void updateTask(Task task) {
-        EntityManager tasksEM = tasksEMF.createEntityManager();
-        tasksEM.getTransaction().begin();
-        
-        tasksEM.merge(task);
-        tasksEM.getTransaction().commit();
-        tasksEM.close();
-    }
-    
-    public void deleteTask(Task task) {
-        EntityManager tasksEM = tasksEMF.createEntityManager();
-        tasksEM.getTransaction().begin();
-        TypedQuery<Task> taskTypedQuery = tasksEM.createQuery("SELECT task FROM Task task WHERE task.id = :selectedId", Task.class);
-        
-        taskTypedQuery.setParameter("selectedId", task.getId());
-        
-        taskTypedQuery.setMaxResults(1);
-        
-        Task taskResult = taskTypedQuery.getSingleResult();
-        
-        tasksEM.remove(taskResult);
-        tasksEM.getTransaction().commit();
-        tasksEM.close();
-    }
-    
-    public List<Task> showAllTasks() {
-        EntityManager tasksEM = tasksEMF.createEntityManager();
-        TypedQuery<Task> taskTypedQuery = tasksEM.createQuery("SELECT task FROM Task task", Task.class);
-        
-        List<Task> tasks = taskTypedQuery.getResultList();
-        
-        tasksEM.close();
-        return tasks;
-    }
-    
-    public void cleanUp() {
-        tasksEMF.close();
-    }
+	private static final EntityManagerFactory tasksEMF = Persistence.createEntityManagerFactory("TasklistPlanner");
+
+	public Task searchForTasksById(int id) {
+		EntityManager tasksEM = tasksEMF.createEntityManager();
+		tasksEM.getTransaction().begin();
+
+		Task foundTask = tasksEM.find(Task.class, id);
+
+		tasksEM.close();
+		return foundTask;
+	}
+
+	public void insertTask(Task task) {
+		EntityManager tasksEM = tasksEMF.createEntityManager();
+		tasksEM.getTransaction().begin();
+
+		tasksEM.persist(task);
+		tasksEM.getTransaction().commit();
+		tasksEM.close();
+	}
+
+	public void updateTask(Task task) {
+		EntityManager tasksEM = tasksEMF.createEntityManager();
+		tasksEM.getTransaction().begin();
+
+		tasksEM.merge(task);
+		tasksEM.getTransaction().commit();
+		tasksEM.close();
+	}
+
+	public void deleteTask(Task task) {
+		EntityManager tasksEM = tasksEMF.createEntityManager();
+		tasksEM.getTransaction().begin();
+		TypedQuery<Task> taskTypedQuery = tasksEM.createQuery("SELECT task FROM Task task WHERE task.id = :selectedId",
+				Task.class);
+
+		taskTypedQuery.setParameter("selectedId", task.getId());
+
+		taskTypedQuery.setMaxResults(1);
+
+		Task taskResult = taskTypedQuery.getSingleResult();
+
+		tasksEM.remove(taskResult);
+		tasksEM.getTransaction().commit();
+		tasksEM.close();
+	}
+
+	public List<Task> showAllTasks() {
+		EntityManager tasksEM = tasksEMF.createEntityManager();
+		tasksEM.getTransaction().begin();
+		List<Task> tasks = (List<Task>) tasksEM.createQuery("SELECT t FROM Task t", Task.class).getResultList();
+		tasksEM.close();
+		return tasks;
+	}
+
+	public void cleanUp() {
+		tasksEMF.close();
+	}
 }
